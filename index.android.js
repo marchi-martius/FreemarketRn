6429310aeeb6d5469edd89fe3205eeef6b6393c0
Mark Volosiuk <marchi.martius@gmail.com>
2017-04-23 13:39:23 +0300

    Initial project...
    ... with minor tweaks :D

---
+++ index.android.js
@@ -0,0 +1,46 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    DrawerLayoutAndroid
+} from 'react-native';
+
+import ProductsList from './app/containers/ProductsList';
+
+export default class FreemarketRn extends Component {
+    renderNavigationView() {
+	return(
+	    <View style={styles.navigationView}>
+	      <Text>Hello World</Text>
+	    </View>
+	);
+    }
+
+    render() {
+	return (
+	    <DrawerLayoutAndroid
+	      drawerWidth={300}
+	      drawerPosition={DrawerLayoutAndroid.positions.left}
+	      renderNavigationView={this.renderNavigationView}>
+	      <ProductsList />
+	    </DrawerLayoutAndroid>
+	);
+    }
+}
+
+const styles = StyleSheet.create({
+    navigationView: {
+	flex: 1,
+	alignItems: 'center'
+    }
+});
+
+AppRegistry.registerComponent('FreemarketRn', () => FreemarketRn);
