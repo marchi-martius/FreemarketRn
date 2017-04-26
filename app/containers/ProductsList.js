2565a9646135b4cd3f45209a9c4100988d31a1c0
obliviusm <obliviusm@gmail.com>
2017-04-27 02:13:06 +0300

    allow to click on category and pass it to list of products

--- app/containers/ProductsList.js
+++ app/containers/ProductsList.js
@@ -4,7 +4,7 @@ import { Text } from 'react-native';
 export default class ProductsList extends Component {
   render() {
     return(
-      <Text>List of products go here</Text>
+      <Text>List of products go here for {this.props.category.name}</Text>
     );
   }
 }
