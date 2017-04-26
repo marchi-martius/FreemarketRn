a751d30f3c7211a96a2e4707c11c2422972dd411
obliviusm <obliviusm@gmail.com>
2017-04-27 01:45:34 +0300

    add static categories

--- app/containers/ProductsList.js
+++ app/containers/ProductsList.js
@@ -4,7 +4,7 @@ import { Text } from 'react-native';
 export default class ProductsList extends Component {
   render() {
     return(
-      <Text>Hello World!</Text>
+      <Text>List of products go here</Text>
     );
   }
 }
--- index.android.js
+++ index.android.js
@@ -10,16 +10,37 @@ import {
   StyleSheet,
   Text,
   View,
-  DrawerLayoutAndroid
+  DrawerLayoutAndroid,
+  ListView
 } from 'react-native';
 
 import ProductsList from './app/containers/ProductsList';
 
+const Categories = [
+  {name: 'Antiques'},
+  {name: 'Books, Comics & Magazines'},
+  {name: 'Cars, Motorcycles & Vehicles'},
+  {name: 'Events Tickets'},
+  {name: 'Pet Supplies'}
+]
+
 export default class FreemarketRn extends Component {
+  constructor() {
+    super();
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: ds.cloneWithRows(Categories)
+    };
+  }
+
   renderNavigationView() {
     return(
       <View style={styles.navigationView}>
-	<Text>Hello World</Text>
+	      <Text>Categories</Text>
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={(rowData) => <Text>{rowData.name}</Text>}
+        />
       </View>
     );
   }
@@ -27,9 +48,9 @@ export default class FreemarketRn extends Component {
   render() {
     return (
       <DrawerLayoutAndroid
-	drawerWidth={300}
-	renderNavigationView={this.renderNavigationView}>
-	<ProductsList />
+        	drawerWidth={300}
+        	renderNavigationView={this.renderNavigationView.bind(this)}>
+      	<ProductsList />
       </DrawerLayoutAndroid>
     );
   }
