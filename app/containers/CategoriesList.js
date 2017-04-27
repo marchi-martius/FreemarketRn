97e06f12992a5f0f4857de0d831cf942faf74146
obliviusm <obliviusm@gmail.com>
2017-04-28 02:10:42 +0300

    move list view

--- app/containers/CategoriesList.js
+++ app/containers/CategoriesList.js
@@ -4,14 +4,14 @@ import { Text, ListView, TouchableHighlight } from 'react-native';
 export default class CategoriesList extends Component {
   constructor(props) {
     super(props);
-    // const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-    // this.props.dataSource = ds.cloneWithRows(this.props.categories);
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.dataSource = ds.cloneWithRows(this.props.categories);
   }
 
   render() {
     return(
       <ListView
-        dataSource={this.props.dataSource}
+        dataSource={this.dataSource}
         renderRow={(rowData, sectionID, rowID) => <TouchableHighlight onPress={this.props.onPressCategory.bind(null, rowID)}><Text>{rowData.name}</Text></TouchableHighlight>}
       />
     );
