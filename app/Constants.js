318eb695493a16f86298da1703c2c59269ca26a7
obliviusm <obliviusm@gmail.com>
2017-04-28 01:47:10 +0300

    extract constants

---
+++ app/Constants.js
@@ -0,0 +1,7 @@
+export const Categories = [
+  {name: 'Antiques'},
+  {name: 'Books, Comics & Magazines'},
+  {name: 'Cars, Motorcycles & Vehicles'},
+  {name: 'Events Tickets'},
+  {name: 'Pet Supplies'}
+]
