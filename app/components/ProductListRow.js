a83e20008b93a84077d383ea95ad138cd338e1f7
obliviusm <obliviusm@gmail.com>
2017-05-13 01:17:39 +0300

    add styling to list

--- app/components/ProductListRow.js
+++ app/components/ProductListRow.js
@@ -2,14 +2,73 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { StyleSheet, Text, View, Image } from 'react-native';
 
+const ident = 10;
+const styles = StyleSheet.create({
+  row: {
+    margin: ident,
+    backgroundColor: 'white',
+    borderRadius: 5,
+    flexDirection: 'row',
+    padding: ident
+  },
+  image: {
+    width: 100,
+    height: 100,
+  },
+  descriptionBlock: {
+    paddingLeft: ident,
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+  },
+  name: {
+    color: 'black',
+    fontSize: 15
+  },
+  numbersBlock: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  timeBlock: {
+
+  },
+  timeTitle: {
+    fontSize: 10
+  },
+  timeValue: {
+    color: 'red',
+    fontSize: 15,
+  },
+  priceBlock: {
+    backgroundColor: 'dimgray',
+    width: 100,
+    borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  price: {
+    color: 'white',
+  }
+});
+
 const ProductListRow = ({ name, image, price }) => (
-  <View>
+  <View style={styles.row}>
     <Image
-      style={{width: 50, height: 50}}
+      style={styles.image}
       source={{uri: image}}
     />
-    <Text>{name}</Text>
-    <Text>{price}</Text>
+    <View style={styles.descriptionBlock}>
+      <Text style={styles.name}>{name}</Text>
+      <View style={styles.numbersBlock}>
+        <View style={styles.timeBlock}>
+          <Text style={styles.timeTitle}>Time Left:</Text>
+          <Text style={styles.timeValue}>17m 5s</Text>
+        </View>
+        <View style={styles.priceBlock}>
+          <Text style={styles.price}>${price}</Text>
+        </View>
+      </View>
+    </View>
   </View>
 );
 
--- app/components/ProductListView.js
+++ app/components/ProductListView.js
@@ -21,7 +21,11 @@ export default class ProductListView extends Component {
 
   render() {
     return (
-      <ListView dataSource={this.dataSource} renderRow={(row) => this.renderRow(row)} />
+      <ListView
+        style={{backgroundColor: 'dimgray'}}
+        dataSource={this.dataSource}
+        renderRow={(row) => this.renderRow(row)}
+      />
     );
   }
 }
@@ -34,34 +38,3 @@ ProductListView.propTypes = {
     price: PropTypes.number.isRequired,
   })).isRequired,
 };
-
-// export default class ProductListView extends Component {
-//   constructor(props) {
-//     super(props);
-//
-//     const ds = new ListView.DataSource({
-//       rowHasChanged: (r1, r2) => r1.id !== r2.id,
-//     });
-//     this.dataSource = ds.cloneWithRows([props.products]);
-//   }
-//
-//   renderRow(row) {
-//     const { name, image, price } = row;
-//     return <ProductListRow {...{ name, image, price }} />;
-//   }
-//
-//   redner() {
-//     return (
-//       <Text>wtf2</Text>
-//     );
-//   }
-// }
-//
-// ProductListView.propTypes = {
-//   products: PropTypes.arrayOf(PropTypes.shape({
-//     id: PropTypes.number.isRequired,
-//     name: PropTypes.string.isRequired,
-//     image: PropTypes.string.isRequired,
-//     price: PropTypes.number.isRequired,
-//   })).isRequired,
-// };
