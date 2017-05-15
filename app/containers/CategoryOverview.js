d90178b71d60cecf3e407e6a77139b22381e0c39
obliviusm <obliviusm@gmail.com>
2017-05-15 14:18:35 +0300

    remove CategoryOverview

--- app/containers/CategoryOverview.js
+++
@@ -1,9 +0,0 @@
-import { connect } from 'react-redux';
-
-import CategoryOverviewView from '../components/CategoryOverviewView';
-
-const mapStateToProps = state => ({
-  category: state.categories.current,
-});
-
-export default connect(mapStateToProps)(CategoryOverviewView);
