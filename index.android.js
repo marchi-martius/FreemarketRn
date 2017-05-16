2c5ea46b540855e6c0d042667cfc61aecd2920d8
obliviusm <obliviusm@gmail.com>
2017-05-16 12:30:39 +0300

    Add empty form

--- index.android.js
+++ index.android.js
@@ -4,10 +4,16 @@ import { Router, Scene } from 'react-native-router-flux'
 
 import App from './app/App';
 import Main from './app/screens/Main.android';
+import ProductForm from './app/screens/ProductForm';
 
-const FreemarketRn = () => {
-  return <App><Main /></App>;
-}
+const FreemarketRn = () => (
+  <App>
+    <Router>
+      <Scene key="ProductIndex" component={Main} hideNavBar />
+      <Scene key="ProductForm" component={ProductForm} hideNavBar initial />
+    </Router>
+  </App>
+);
 export default FreemarketRn;
 
 AppRegistry.registerComponent('FreemarketRn', () => FreemarketRn);
