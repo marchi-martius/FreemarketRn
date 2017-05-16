1bf6be8be2416dccc9043a0c45d4977f804ee208
obliviusm <obliviusm@gmail.com>
2017-05-16 12:13:00 +0300

    Add react-native-router-flux and required packages for it

--- index.android.js
+++ index.android.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { AppRegistry } from 'react-native';
+import { Router, Scene } from 'react-native-router-flux'
 
 import App from './app/App';
 import Main from './app/screens/Main.android';
 
-const FreemarketRn = () => <App><Main /></App>;
+const FreemarketRn = () => {
+  return <App><Main /></App>;
+}
 export default FreemarketRn;
 
 AppRegistry.registerComponent('FreemarketRn', () => FreemarketRn);
