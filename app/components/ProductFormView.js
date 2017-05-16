e3fdd40cb1a247322a75e7c44366c85c32e0f30f
obliviusm <obliviusm@gmail.com>
2017-05-16 17:27:38 +0300

    use showImagePicker. Image is selected but not displayed

--- app/components/ProductFormView.js
+++ app/components/ProductFormView.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { View, Button } from 'react-native';
+import { View, Button, Image, Text } from 'react-native';
 import t from 'tcomb-form-native';
+import ImagePicker from 'react-native-image-picker';
 
 const Form = t.form.Form;
 
@@ -10,9 +11,61 @@ const Product = t.struct({
 });
 
 export default class ProductForm extends Component {
+  state = {
+    avatarSrouce: null,
+  };
+
+  selectPhotoTapped() {
+    const options = {
+      quality: 1.0,
+      maxWidth: 500,
+      maxHeight: 500,
+      storageOptions: {
+        skipBackup: true
+      }
+    };
+
+    ImagePicker.showImagePicker(options, (response) => {
+      console.log('Response = ', response);
+
+      if (response.didCancel) {
+        console.log('User cancelled photo picker');
+      }
+      else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      }
+      else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+      }
+      else {
+        let source = { uri: response.uri };
+
+        // You can also display the image using data:
+        // let source = { uri: 'data:image/jpeg;base64,' + response.data };
+
+        this.setState({
+          avatarSource: source
+        });
+      }
+    });
+  }
+
+  renderPhoto() {
+    if (this.state.avatarSource) {
+      return <Image source={this.state.avatarSource} />;
+    } else {
+      return <Text>Select a Photo</Text>;
+    }
+  }
+
   render() {
     return (
       <View>
+        { this.renderPhoto() }
+        <Button
+          title="Add Image"
+          onPress={() => this.selectPhotoTapped()}
+        />
         <Form
           ref={ _form => this.form = _form }
           type={Product}
