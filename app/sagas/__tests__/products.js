868642bdd4be31629732f831fd270c1e4942a097
obliviusm <obliviusm@gmail.com>
2017-05-18 02:23:31 +0300

    Add Saga tests

--- app/sagas/__tests__/products.js
+++ app/sagas/__tests__/products.js
@@ -1,8 +1,16 @@
 import { call, put } from 'redux-saga/effects';
+import { Actions } from 'react-native-router-flux';
 
-import { fetchProducts } from '../products';
-import { productsSuccess, productsError } from '../../reducers/products';
-import { fetchAllProducts } from '../../Api';
+import { fetchProducts, createProduct } from '../products';
+import {
+  productsSuccess,
+  productsError,
+
+  createProductSuccess,
+  createProductError,
+} from '../../reducers/products';
+import * as Api from '../../Api';
+import * as FirebaseApi from '../../FirebaseApi';
 
 describe('fetchProducts', () => {
   it('calls success action on resolve', () => {
@@ -10,7 +18,7 @@ describe('fetchProducts', () => {
     const products = [];
     const response = { data: products };
 
-    expect(gen.next().value).toEqual(call(fetchAllProducts));
+    expect(gen.next().value).toEqual(call(Api.fetchAllProducts));
     expect(gen.next(response).value).toEqual(put(productsSuccess(products)));
   });
 
@@ -18,7 +26,33 @@ describe('fetchProducts', () => {
     const gen = fetchProducts();
     const error = {};
 
-    expect(gen.next().value).toEqual(call(fetchAllProducts));
+    expect(gen.next().value).toEqual(call(Api.fetchAllProducts));
     expect(gen.throw(error).value).toEqual(put(productsError(error)));
+  });
+});
+
+describe('createProduct', () => {
+  it('calls success action on resolve', () => {
+    const product = {};
+    const payload = {localImage: true, product};
+    const image = "image src";
+
+    const gen = createProduct({payload});
+
+    expect(gen.next().value).toEqual(call(FirebaseApi.saveImageToStorage, payload.localImage));
+    expect(gen.next(image).value).toEqual(call(Api.createProduct, {...product, image}));
+    expect(gen.next().value).toEqual(put(createProductSuccess()));
+  });
+
+  it('calls error on failure', () => {
+    const product = {};
+    const payload = {localImage: true, product};
+    const image = "image src";
+
+    const gen = createProduct({payload});
+    const error = {};
+
+    expect(gen.next().value).toEqual(call(FirebaseApi.saveImageToStorage, payload.localImage));
+    expect(gen.throw(error).value).toEqual(put(createProductError(error)));
   });
 });
